cc1266e8d4568365222dae5720038beae0e57d5f
bubby3854 <168155701+bubby3854@users.noreply.github.com>
2024-07-12 14:43:22 +0900

    Delete simpleBbs directory

--- simpleBbs/src/main/java/com/example/simpleBbs/repository/SimpleBbsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.simpleBbs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.simpleBbs.entity.SimpleBbs;
-
-public interface SimpleBbsRepository extends JpaRepository<SimpleBbs, Long> {
-	
-}
